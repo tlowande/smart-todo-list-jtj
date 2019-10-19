9cbefd3e65b5e75fd50e97b050bb2cb60f8a7e0d
Tamires <tamires.lowande@gmail.com>
2019-10-19 16:27:21 -0400

    added categorizeByVerb function

---
+++ helpers/Tamires_functions.js
@@ -0,0 +1,24 @@
+/**
+ * Get the user input and filter if there's a verb on that string by using our constructed object
+ * @param {String}task The user input
+ * @param {Object}obj The obj created by us
+ * @return {Boolean}
+ */
+
+const verbs = {
+  watch: { 1: 'movies' },
+  read: { 2: 'books' },
+  buy: { 4: 'products' },
+  order: { 4: 'products' }
+};
+
+const categorizeByVerb = function (task ) {
+  const key = task.toLowerCase().split(' ')
+  if (key.length > 1 && verbs.hasOwnProperty(key[0])) {
+    return verbs.[key[0]]
+  }
+  return false
+
+}
+
+console.log(categorizeByVerb('read harry potter'))
