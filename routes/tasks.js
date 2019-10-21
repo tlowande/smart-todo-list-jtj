d5a700600f3a8296353787dbdc60aaeb1b773a50
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-10-21 19:10:20 -0400

    created get /task route with cookies (#23)

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const {
+  getTaskById,
+  getUserById
+ } = require('../helpers/database')
 
 module.exports = () => {
   // load tasks page
-  router.get('/', (req, res) => {
+  router.get('/', async (req, res) => {
+    const user = await getUserById(req.session.user_id);
+    const task = await getTaskById(req.session.user_id);
+    //returns an array of objects
     let templateVars = {
-      //
-
+      user: user,
+      task: task
     }
-    res.render('../views/tasks-test');
+    res.render('../views/tasks-test', templateVars);
   })
-
-
-
+  return router;
 }
