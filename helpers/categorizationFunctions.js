df7107770efa333fb1f006a4beec9d9c956b6464
jcarrera94 <54748896+jcarrera94@users.noreply.github.com>
2019-10-21 15:57:12 -0400

    Feature/jc #8 final sortfn (#21)
    * package-lock.json was modified
    
    * adds comments to functions within directory and deletes commented out code
    
    * finish final categorize function which sorts and adds the new task into the db when called
    
    * Reorganize all functions that helps categorize a task into one file
    
    * Delete all files with redundant code within helpers directory
    
    * change name of file to match function name (categorizeTask)
    
    * Move categorizeTask function into file containing all categorizing helper functions
    
    * delete categorizeTask file since it is now redundant
    
    * removed extra 'async' from getSearchResults() as it was redundant

---
+++ helpers/categorizationFunctions.js
@@ -0,0 +1,146 @@
+const { addTask } = require('./addTask');
+const SerpWow = require('google-search-results-serpwow');
+// create the serpwow object
+const serpwow = new SerpWow('02726F437EFB47BAABF8368C718AFD64');
+
+/**
+ * 1 - Pre-defined tags contining the keywords used to categorize the task
+ * 2 - Building the regex with tags
+ * 3 - Combines all regex into object to pass into function to categorize the task
+ */
+// 1
+const movieTags = ['film', 'directed', 'imdb', 'starring', 'television', 'netflix', 'tv', 'sitcom'];
+const bookTags = ['published', 'novel', 'author', 'non-fiction', 'book', 'paperback', 'novels'];
+const foodTags = ['menu', 'yelp', 'foodora', 'restaurant'];
+// 2
+const movieRegex = buildRegex(movieTags);
+const bookRegex = buildRegex(bookTags);
+const foodRegex = buildRegex(foodTags);
+// 3
+const regexObj = {
+  1: movieRegex,
+  2: bookRegex,
+  3: foodRegex
+};
+
+/**
+ * returns regex as an object
+ * @param {Array} keywords for each specific category
+ * @return {Object} Object with regex
+ */
+function buildRegex(tags) {
+  return new RegExp("\\b(" + tags.join('|') + ")\\b", "ig")
+}
+
+/**
+ * Get the user input and filter if there's a verb on that string by using our constructed object
+ * @param {String}task The user input
+ * @return {Boolean}false||{CategoryID}
+ */
+const categorizeByVerb = function (task) {
+  const verbs = {
+    watch: { 1: 'movies' },
+    read: { 2: 'books' },
+    order: { 2: 'restaurants' },
+    buy: { 4: 'products' }
+  };
+
+  const key = task.toLowerCase().split(' ')
+  if (key.length > 1 && verbs.hasOwnProperty(key[0])) {
+    return Number(Object.keys(verbs[key[0]]));
+  }
+  return false
+}
+
+/**
+ * return top 10 organic search results from google
+ * https://www.npmjs.com/package/google-search-results-serpwow
+ * @param {String} what the user inputs
+ * @return {[Objects]} what the function returns
+ */
+const getSearchResults = (queryString) => {
+  return new Promise(async (resolve, reject) => {
+    let result = await serpwow.json({
+      q: queryString,
+      gl: 'ca',
+      hl: 'en',
+      location: 'Toronto,Ontario,Canada',
+      google_domain: 'google.ca'
+    });
+    try {
+      resolve(result.organic_results);
+    } catch(err) {
+      reject(err.message);
+    }
+  });
+}
+
+/**
+ * returns string with all results of API call
+ * @param {Array} objects of top 10 google search
+ * @return {String} to use in next function
+ */
+const combineResults = (arr) => {
+  let str = '';
+  for (let obj of arr) {
+    str += str.concat(' ', obj.title.concat(' ', obj.snippet));
+  }
+  return str.toLowerCase();
+}
+
+/**
+ * Returns category id
+ * @param {String} final results from api results to compare against specific keywords
+ * @return {Number} category_id
+ */
+const getCategory = function(string) {
+  let arr = [];
+  result = null;
+
+  for (const regex in regexObj) {
+    if (string.match(regexObj[regex]) && string.match(regexObj[regex]).length > arr.length) {
+     arr = string.match(regexObj[regex]);
+     result = Number(regex);
+    }
+  }
+  if (!result) {
+    result = 4;
+  }
+  return result;
+}
+
+/**
+ * Calls addTask function and inserts task into database
+ * @param {String} task from user input
+ * @return nothing yet..??? maybe something later
+ */
+const categorizeTask = (task) => {
+  const firstScreen = categorizeByVerb(task)
+  if (firstScreen) {
+    console.log('before API', firstScreen);
+    // addTask(task, user_id, firstScreen);
+    return
+    //calls function that renders new task already categorized in user's main page
+  } else {
+    getSearchResults(userInput)
+    .then(res => {
+      return combineResults(res);
+    })
+    .then(res => {
+      return getCategory(res);
+    })
+    .then(res => {
+      console.log('After API:', res);
+      // addTask(task, user, res);
+      //calls function that renders new task already categorized in user's main page
+    })
+    .catch(err => {
+      console.error('query error', err.stack);
+    });
+  }
+}
+
+userInput = 'osmows';
+user = 2;
+categorizeTask(userInput);
+
--- helpers/combineResults.js
+++
@@ -1,9 +0,0 @@
-const combineResults = (arr) => {
-  let str = '';
-  for (let obj of arr) {
-    str += str.concat(' ', obj.title.concat(' ', obj.snippet));
-  }
-  return str.toLowerCase();
-}
-
-module.exports = { combineResults };
