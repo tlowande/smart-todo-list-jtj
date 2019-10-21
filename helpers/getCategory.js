9e1eaa9c1b92877755d0bbdf5213c4f89f4c6a32
Juan Carrera <linaarivas@Linas-MacBook-Air.local>
2019-10-21 10:30:20 -0400

    categorize input into either category using Regex

---
+++ helpers/getCategory.js
@@ -0,0 +1,40 @@
+const movieTags = ['film', 'directed', 'imdb', 'starring', 'television', 'netflix', 'series'];
+const bookTags = ['published', 'novel', 'author', 'non-fiction', 'book', 'paperback', 'novels'];
+const foodTags = ['menu', 'yelp', 'foodora', 'restaurant'];
+
+/**
+ * returns regex as an object
+ * @param {Array} keywords for each specific category
+ * @return {category_id} what the function returns
+ */
+function buildRegex(tags) {
+  return new RegExp("\\b(" + tags.join('|') + ")\\b", "ig")
+}
+
+const movieRegex = buildRegex(movieTags);
+const bookRegex = buildRegex(bookTags);
+const foodRegex = buildRegex(foodTags);
+
+const regObj = {
+  1: movieRegex,
+  2: bookRegex,
+  3: foodRegex
+};
+
+const getCategory = function(regexObj, string) {
+  let arr = [];
+  result = null;
+
+  for (const regex in regexObj) {
+    if (string.match(regexObj[regex]) && string.match(regexObj[regex]).length > arr.length) {
+     arr = string.match(regexObj[regex]);
+     result = Number(regex);
+    }
+  }
+  if (!result) {
+    result = 4;
+  }
+  return result;
+}
+
+console.log(getCategory(regObj, str));
