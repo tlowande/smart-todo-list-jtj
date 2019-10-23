55ff355117bf277c6cc9b0cd335596807ca0d7ab
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-10-23 10:46:50 -0400

    renderTasks function in loadTasks.js completed

--- public/scripts/loadTasks.js
+++ public/scripts/loadTasks.js
@@ -1,43 +1,55 @@
-// genereate markup for a single task
-// obj = { task, category_id }
-const createTaskElement = (task) => {
-  const $taskCard = $('<ol>').addClass('task');
-  const markup = `
-    <li>${task.task}</li>
-  `;
-  const result = {
-    task: $taskCard.append(markup),
-    category_id: task.category_id
-  };
-  return result;
-}
-
-// render multiple tasks
-const renderTasks = (tasks) => {
-  // initialize empty array to store rendered tasks
-  const renderedTasks = [];
-  // check which category container to append the task to
-  switch (obj.category_id) {
-    case 1:
-      const $taskContainer = $('#movies');
-      break;
-    case 2:
-      const $taskContainer = $('#books');
-      break;
-    case 3:
-      const $taskContainer = $('#restaurants');
-      break;
-    default:
-      const $taskContainer = $('#products');
-  }
-
-  for (let task of tasks) {
-    renderedTasks.shift(createTaskElement(task)[0].outerHTML);
-  }
-  $taskContainer.prepend(renderedTasks.join(''));
-}
+// Client Side jQuery logic
+// const { $ } = require("../vendor/jquery-3.0.0");
 
 // get tasks from database
+
+// render multiple tasks
+// tasks = [ {task, category_id} ]
+const renderTasks = (tasks) => {
+  // initialize empty array to store rendered tasks
+  const renderedTasks_movies = [];
+  const renderedTasks_books = [];
+  const renderedTasks_restaurants = [];
+  const renderedTasks_products = [];
+  // initialize taskContainers
+  const $taskContainer_movies = $('#movies');
+  const $taskContainer_books = $('#books');
+  const $taskContainer_restaurants = $('#restaurants');
+  const $taskContainer_products = $('#products');
+
+  // wrap each task in html and add to corresponding array
+  for (task of tasks) {
+    // check which category container to append the task to
+    switch (task.category_id) {
+      case 1:
+        renderedTasks_movies.shift(createTaskElement(task.task).outerHTML);
+        break;
+      case 2:
+        renderedTasks_books.shift(createTaskElement(task.task).outerHTML);
+        break;
+      case 3:
+        renderedTasks_restaurants.shift(createTaskElement(task.task).outerHTML);
+        break;
+      default:
+        renderedTasks_products.shift(createTaskElement(task.task).outerHTML);
+    }
+  }
+
+  $taskContainer_movies.prepend(renderedTasks_movies.join(''));
+  $taskContainer_books.prepend(renderedTasks_books.join(''));
+  $taskContainer_restaurants.prepend(renderedTasks_restaurants.join(''));
+  $taskContainer_products.prepend(renderedTasks_products.join(''));
+}
+
+// genereate markup for a single task
+// task = task
+const createTaskElement = (task) => {
+  const $taskCard = $('<ul>').addClass('task');
+  const markup = `
+    <li>${task}</li>
+  `;
+  return $taskCard.append(markup);
+}
 
 
 
