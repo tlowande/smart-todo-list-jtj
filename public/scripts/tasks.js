e651b289fa6bac63d6fcffb8b6da2f42fc7e1572
Tamires <tamires.lowande@gmail.com>
2019-10-23 11:11:58 -0400

    first response from drag and drop feature

--- public/scripts/tasks.js
+++ public/scripts/tasks.js
@@ -28,3 +28,18 @@ $(() => {
 
 });
 
+
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
